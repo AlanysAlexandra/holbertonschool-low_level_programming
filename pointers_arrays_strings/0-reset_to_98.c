595aee4f4e893892fa178c181834ef3072e161f9
AlanysAlexandra <alanys.alexandra@icloud.com>
2023-03-01 00:10:50 +0000

    takes a pointer ti a parameter and updates its value to 98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
- * reset_to_98(int *n); -takes a pointer to an int as parameter and updates the value it points to 98 
+ * reset_to_98(int *n); -takes a pointer  as parameter and updates the value 
  *
  * Return: No return
  */
 void reset_to_98(int *n)
 {
 	*n = 98;
-	
 }
